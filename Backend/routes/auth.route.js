1ef1006eac02fca0878c853b7227d0bf6520e53b
careerkings <careerkingsconsult@gmail.com>
2024-04-10 01:49:33 +0100

    my new commit

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,8 +1,11 @@
-import express  from "express"
-import signup from "../controllers/auth.controller.js";
+import express from 'express';
+import { google, signOut, signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-const authRouter =  router.post('/signup', signup)
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post('/google', google);
+router.get('/signout', signOut)
 
- export default authRouter
+export default router;
